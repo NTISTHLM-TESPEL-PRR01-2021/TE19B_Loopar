d4f9095931d340fbab7a56476438a403f646425d
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-30 17:39:34 +0200

    Initial commit

---
+++ Loopar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Loopar
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Skriv A:");
+
+      string s = "";
+
+      while (s != "A" && s != "B" && s != "C")
+      {
+        s = Console.ReadLine();
+
+        if (s != "A" && s != "B" && s != "C")
+        {
+          Console.WriteLine("NO U STUPID! ONLY A B OR C ALLOWED");
+        }
+
+      }
+
+      Console.WriteLine("WELL DONE!");
+
+
+      Console.ReadLine();
+
+    }
+  }
+}
